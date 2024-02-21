5015f08d920c508581a10aa3b501f1e38e0944a6
Younes202 <younessghyer642@gmail.com>
2024-02-21 13:25:09 +0100

    passing  images in detailsproduct

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,10 @@
-// index.js
-
 import { createRouter, createWebHistory } from 'vue-router';
 import DetailsProduct from '../views/DetailsProduct.vue';
-import List_Products from '../views/List_Products.vue'; // Corrected import path
+import List_Products from '../views/List_Products.vue';
 
 const routes = [
   { path: '/', component: List_Products },
-  { path: '/details/:id', name: 'details', component: DetailsProduct, props: true },
+  { path: '/details/:id', name: 'details', component: DetailsProduct },
 ];
 
 const router = createRouter({
